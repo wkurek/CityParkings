b40e2f0bf1a103568568fd2bb388be0003a9c62c
jszachewicz <jszachewicz@vewd.com>
2018-12-28 16:33:46 +0100

    WIP/edit

--- src/controller/ParkController.java
+++ src/controller/ParkController.java
@@ -11,8 +11,14 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import model.park.Park;
 import model.park.ParkDAO;
+import model.parking.Parking;
+import model.parking.ParkingDAO;
 import model.user.User;
 import model.user.UserDAO;
+import model.vehicle.Vehicle;
+import model.vehicle.VehicleDAO;
+
+import java.sql.Date;
 
 public class ParkController {
     private ObservableList<Park> parksList;
@@ -138,32 +144,77 @@ public class ParkController {
         scheduleLoadTask(parksLoadTask);
     }
 
+    @FXML
     private void onParkSelected(Park selectedPark) {
+        deleteParkButton.setDisable(false);
+        saveParkButton.setDisable(true);
+        editParkButton.setDisable(false);
 
+        parkVehicleIdInput.setText(Integer.toString(selectedPark.getVehicle().getId()));
+        parkParkingIdInput.setText(Integer.toString(selectedPark.getParking().getParkingId()));
+        parkDateInput.setText(selectedPark.getDateTime().toString());
+
+        parkStandardLotsNumerInput.setText(String.valueOf(selectedPark.getParking().getStandardLots()));
+        parkDisabledParkLotsInput.setText(String.valueOf(selectedPark.getParking().getDisabledLots()));
+        parkingIsGuardedInput.setText(Boolean.toString(selectedPark.getParking().isGuarded()));
+        parkingIsRoofedInput.setText(Boolean.toString(selectedPark.getParking().isRoofed()));
+        parkMaxHeightInput.setText(Float.toString(selectedPark.getParking().getMaxHeight()));
+        parkMaxWeightInput.setText(Float.toString(selectedPark.getParking().getMaxWeight()));
+        parkLongitudeInput.setText(Float.toString(selectedPark.getParking().getLocation().getLatitude()));
+        parkParkLatitudeInput.setText(Float.toString(selectedPark.getParking().getLocation().getLatitude()));
     }
 
     @FXML
     void onDeleteParkButtonClicked(ActionEvent event) {
+        Park selectedPark = parksTableView.getSelectionModel().getSelectedItem();
+        int selectedParkIndex = parksTableView.getSelectionModel().getFocusedIndex();
 
+        if(selectedPark != null && selectedParkIndex >= 0) {
+            ParkDAO.deletePark(selectedPark.getDateTime().toString(), selectedPark.getVehicle().getId());
+            parksList.remove(selectedParkIndex);
+        } else {
+            deleteParkButton.setDisable(true);
+        }
+    }
+
+    private void setParkInputFieldDistable(boolean distable) {
+        parkVehicleIdInput.setDisable(distable);
+        parkParkingIdInput.setDisable(distable);
+        parkDateInput.setDisable(distable);
     }
 
     @FXML
     void onEditParkButtonClicked(ActionEvent event) {
+        editParkButton.setDisable(true);
+        saveParkButton.setDisable(false);
 
+        setParkInputFieldDistable(false);
     }
 
     @FXML
     void onNewParkButtonClicked(ActionEvent event) {
-
+        //TODO: invoke popup with UserForm
     }
 
     @FXML
     void onRefreshButtonClicked(ActionEvent event) {
-
+        scheduleLoadTask(parksLoadTask);
     }
 
     @FXML
     void onSaveParkButtonClicked(ActionEvent event) {
+        int editedParkIndex = parksTableView.getSelectionModel().getSelectedIndex();
+        Park editedPark = parksList.get(editedParkIndex);
+
+        Vehicle editedVehicle = VehicleDAO.getVehicle(Integer.valueOf(parkVehicleIdInput.getText()));
+        Parking editedParking = ParkingDAO.getParking(Integer.valueOf(parkParkingIdInput.getText()));
+        editedPark.setVehicle(editedVehicle);
+        editedPark.setParking(editedParking);
+        editedPark.setDateTime(Date.valueOf(parkDateInput.getText()));
+
+        editParkButton.setDisable(false);
+        saveParkButton.setDisable(true);
+        setParkInputFieldDistable(true);
 
     }
 
