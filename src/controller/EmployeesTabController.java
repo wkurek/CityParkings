5ae3f0e00eb723eb491d838fcf1dc93391503d8c
MBelniak <rubik40s@wp.pl>
2019-01-04 23:58:57 +0100

    EmployeeTab almost done

--- src/controller/EmployeesTabController.java
+++ src/controller/EmployeesTabController.java
@@ -1,16 +1,17 @@
 package controller;
 
+
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckMenuItem;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import model.country.Country;
 import model.country.CountryDAO;
 import model.department.Department;
 import model.department.DepartmentDAO;
+import model.views.EmployeesView;
 import model.views.EmployeesViewDAO;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +24,8 @@ public class EmployeesTabController {
     @FXML
     public MenuButton columnMenuButton;
     @FXML
+    public TableView<EmployeesView> employeesTable;
+    @FXML
     private TextField salaryMinInput;
     @FXML
     private TextField salaryMaxInput;
@@ -30,6 +33,7 @@ public class EmployeesTabController {
     private List<CheckMenuItem> departmentItems;
     private List<CheckMenuItem> columnItems;
 
+    private List<TableColumn> columns;
 
     public EmployeesTabController()
     {
@@ -59,14 +63,106 @@ public class EmployeesTabController {
         }
         columnMenuButton.getItems().setAll(columnItems);
 
+        autoShow();
+
+        generateTableColumns();
+        setColumns();
+
+    }
+
+    @FXML
+    private void autoShow() {
 
 
+        for(int i = 0; i<countryMenuButton.getItems().size(); i++)
+            countryMenuButton.getItems().get(i).setOnAction(e -> e.consume());
+        for(int i = 0; i<columnMenuButton.getItems().size(); i++)
+            columnMenuButton.getItems().get(i).setOnAction(e -> e.consume());
+        for(int i = 0; i<departmentMenuButton.getItems().size(); i++)
+            departmentMenuButton.getItems().get(i).setOnAction(e -> e.consume());
     }
     @FXML
     public void onEmployeesFilterClicked()
     {
 
     }
+
+    private void generateTableColumns()
+    {
+        columns = new ArrayList<>();
+        TableColumn<EmployeesView, Integer> ID = new TableColumn<>("ID");
+        ID.prefWidthProperty().bind(employeesTable.widthProperty().divide(19));
+        ID.setCellValueFactory(param->param.getValue().idProperty().asObject());
+        columns.add(ID);
+        TableColumn<EmployeesView, String> name= new TableColumn<>("Name");
+        name.prefWidthProperty().bind(employeesTable.widthProperty().divide(19/2));
+        name.setCellValueFactory(param->param.getValue().nameProperty());
+        columns.add(name);
+        TableColumn<EmployeesView, String> surname = new TableColumn<>("Surname");
+        surname.prefWidthProperty().bind(employeesTable.widthProperty().divide(19/2));
+        surname.setCellValueFactory(param->param.getValue().surnameProperty());
+        columns.add(surname);
+        TableColumn<EmployeesView, Float> salary = new TableColumn<>("Salary");
+        salary.prefWidthProperty().bind(employeesTable.widthProperty().divide(19));
+        salary.setCellValueFactory(param->param.getValue().salaryProperty().asObject());
+        columns.add(salary);
+        TableColumn<EmployeesView, String> country= new TableColumn<>("Country");
+        country.prefWidthProperty().bind(employeesTable.widthProperty().divide(19/2));
+        country.setCellValueFactory(param->param.getValue().getAddress().getCountry().nameProperty());
+        columns.add(country);
+        TableColumn<EmployeesView, String> city = new TableColumn<>("City");
+        city.prefWidthProperty().bind(employeesTable.widthProperty().divide(19/2));
+        city.setCellValueFactory(param->param.getValue().getAddress().cityProperty());
+        columns.add(city);
+        TableColumn<EmployeesView, String> ZIPcode = new TableColumn<>("ZIP Code");
+        ZIPcode.prefWidthProperty().bind(employeesTable.widthProperty().divide(19));
+        ZIPcode.setCellValueFactory(param->param.getValue().getAddress().zipCodeProperty());
+        columns.add(ZIPcode);
+        TableColumn<EmployeesView, String> street = new TableColumn<>("Street");
+        street.prefWidthProperty().bind(employeesTable.widthProperty().divide(19/2));
+        street.setCellValueFactory(param->param.getValue().getAddress().streetProperty());
+        columns.add(street);
+        TableColumn<EmployeesView, String> number = new TableColumn<>("Number");
+        number.prefWidthProperty().bind(employeesTable.widthProperty().divide(19));
+        number.setCellValueFactory(param->param.getValue().getAddress().numberProperty());
+        columns.add(number);
+        TableColumn<EmployeesView, String> department = new TableColumn<>("Department");
+        department.prefWidthProperty().bind(employeesTable.widthProperty().divide(19/2));
+        department.setCellValueFactory(param->param.getValue().getDepartment().departmentNameProperty());
+        columns.add(department);
+        TableColumn<EmployeesView, Integer> parkingID= new TableColumn<>("Parking ID");
+        parkingID.prefWidthProperty().bind(employeesTable.widthProperty().divide(19));
+        parkingID.setCellValueFactory(param->param.getValue().parking_idProperty().asObject());
+        columns.add(parkingID);
+        TableColumn<EmployeesView, Date> lastControl = new TableColumn<>("Last Control Date");
+        lastControl.prefWidthProperty().bind(employeesTable.widthProperty().divide(19/2));
+        lastControl.setCellValueFactory(param->param.getValue().lastControlProperty());
+        columns.add(lastControl);
+    }
+     public void setColumns()
+    {
+        employeesTable.getColumns().clear();
+        if(((CheckMenuItem)columnMenuButton.getItems().get(0)).isSelected())
+        {
+            for(int i = 0; i<columns.size();i++) {
+                employeesTable.getColumns().add(columns.get(i));
+            }
+            return;
+        }
+        for(int i = 1; i<columns.size();i++)
+        {
+            if(((CheckMenuItem)columnMenuButton.getItems().get(i)).isSelected())
+                employeesTable.getColumns().add(columns.get(i-1));
+        }
+        if(employeesTable.getColumns().size()==0)
+        {
+            for(int i = 0; i<columns.size();i++) {
+                employeesTable.getColumns().add(columns.get(i));
+            }
+            return;
+        }
+    }
+
 
 }
 
--- src/model/views/EmployeesView.java
+++ src/model/views/EmployeesView.java
@@ -25,4 +25,92 @@ public class EmployeesView {
     public EmployeesView() {
 
     }
+
+    public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getSurname() {
+        return surname.get();
+    }
+
+    public StringProperty surnameProperty() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname.set(surname);
+    }
+
+    public float getSalary() {
+        return salary.get();
+    }
+
+    public FloatProperty salaryProperty() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary.set(salary);
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public int getParking_id() {
+        return parking_id.get();
+    }
+
+    public IntegerProperty parking_idProperty() {
+        return parking_id;
+    }
+
+    public void setParking_id(int parking_id) {
+        this.parking_id.set(parking_id);
+    }
+
+    public Date getLastControl() {
+        return lastControl.get();
+    }
+
+    public SimpleObjectProperty<Date> lastControlProperty() {
+        return lastControl;
+    }
+
+    public void setLastControl(Date lastControl) {
+        this.lastControl.set(lastControl);
+    }
 }
